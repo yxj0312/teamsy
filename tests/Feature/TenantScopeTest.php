02e0949af1ee18972edf1267afcf312f6a1d4039
yxj0312 <yanxiajun@hotmail.com>
2020-11-07 17:16:48 +0100

    config phpunit

---
+++ tests/Feature/TenantScopeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TenantScopeTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    /**
+     * @test
+     */
+    public function a_model_has_a_tenant_id_on_the_migration()
+    {
+        $this->assertTrue(true);
+    }
+
+
+}
