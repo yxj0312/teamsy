c93a2ca6bc25912a2a6051618c5a2dd5ad8d5965
yxj0312 <yanxiajun@hotmail.com>
2020-11-07 23:41:22 +0100

    tenantScope test

--- tests/Feature/TenantScopeTest.php
+++ tests/Feature/TenantScopeTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use File;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,7 +17,18 @@ class TenantScopeTest extends TestCase
      */
     public function a_model_has_a_tenant_id_on_the_migration()
     {
-        $this->assertTrue(true);
+        $now = now();
+        $this->artisan('make:model Test -m');
+        // find the migration file and check it has a tenant_id on it
+        $filename = $now->year . '_' . $now->format('m') . '_' . $now->format('d') . '_' . $now->format('h') . $now->format('i') . $now->format('s')
+            . '_create_tests_table.php';
+        dump(database_path('migrations/' . $filename));
+        dd(File::exists(database_path('migrations/' . $filename)));
+        $this->assertTrue(File::exists(database_path('migrations/' . $filename)));
+
+        // clean up
+        //File::delete(database_path('migrations/' . $filename));
+        //File::delete(app_path('Models/Test.php'));
     }
 
 
