b3787929cfe5b8705b57a71714ca2a5c88193d7f
yxj0312 <yanxiajun@hotmail.com>
2020-11-08 21:03:02 +0100

    Change format of hour

--- tests/Feature/TenantScopeTest.php
+++ tests/Feature/TenantScopeTest.php
@@ -2,11 +2,11 @@
 
 namespace Tests\Feature;
 
-use File;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\File;
 use Tests\TestCase;
-
+use Str;
 class TenantScopeTest extends TestCase
 {
     use RefreshDatabase;
@@ -17,19 +17,16 @@ class TenantScopeTest extends TestCase
      */
     public function a_model_has_a_tenant_id_on_the_migration()
     {
-        $now = now();
+        $now = now('Europe/Berlin');
         $this->artisan('make:model Test -m');
+        
         // find the migration file and check it has a tenant_id on it
-        $filename = $now->year . '_' . $now->format('m') . '_' . $now->format('d') . '_' . $now->format('h') . $now->format('i') . $now->format('s')
+        $filename = $now->year . '_' . $now->format('m') . '_' . $now->format('d') . '_' . $now->format('H') . $now->format('i') . $now->format('s')
             . '_create_tests_table.php';
-        dump(database_path('migrations/' . $filename));
-        dd(File::exists(database_path('migrations/' . $filename)));
-        $this->assertTrue(File::exists(database_path('migrations/' . $filename)));
+        $this->assertTrue(File::exists(database_path('migrations\\' . $filename)));
 
         // clean up
-        //File::delete(database_path('migrations/' . $filename));
-        //File::delete(app_path('Models/Test.php'));
+        File::delete(database_path('migrations\\' . $filename));
+        File::delete(app_path('Models/Test.php'));
     }
-
-
 }
