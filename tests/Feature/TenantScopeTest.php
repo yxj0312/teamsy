e6e9ab1b9744e697d3ebd807a996c5177f342e9b
yxj0312 <yanxiajun@hotmail.com>
2020-11-08 23:24:00 +0100

    test stub by migration.create.stub

--- tests/Feature/TenantScopeTest.php
+++ tests/Feature/TenantScopeTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Tenant;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\File;
@@ -19,14 +21,36 @@ class TenantScopeTest extends TestCase
     {
         $now = now('Europe/Berlin');
         $this->artisan('make:model Test -m');
-        
+
         // find the migration file and check it has a tenant_id on it
         $filename = $now->year . '_' . $now->format('m') . '_' . $now->format('d') . '_' . $now->format('H') . $now->format('i') . $now->format('s')
             . '_create_tests_table.php';
         $this->assertTrue(File::exists(database_path('migrations\\' . $filename)));
-
+        $this->assertStringContainsString('$table->unsignedBigInteger(\'tenant_id\')->index();',
+            File::get(database_path('migrations\\' . $filename)));
         // clean up
         File::delete(database_path('migrations\\' . $filename));
         File::delete(app_path('Models/Test.php'));
     }
+
+    /** @test */
+    function a_user_can_only_see_users_in_the_same_tenant()
+    {
+        $tenant1 = Tenant::factory()->create();
+        $tenant2 = Tenant::factory()->create();
+
+        $user1 = User::factory()->create(['tenant_id' => $tenant1]);
+
+        User::factory()->count(9)->create(['tenant_id' => $tenant1]);
+        User::factory()->count(10)->create(['tenant_id' => $tenant2]);
+
+        auth()->login($user1);
+        $this->assertEquals(10, User::count());
+    }
+
+    /** @test */
+    public function test_a_user_can_only_create_a_user_in_his_tenant(){
+
+    }
+
 }
