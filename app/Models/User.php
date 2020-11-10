3b2001ab1474c78d7caa57c03ff8e88c9d4fc57f
yxj0312 <yanxiajun@hotmail.com>
2020-11-10 22:39:00 +0100

    refactor codes, using trait instead of booted method in the model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,7 @@
 
 namespace App\Models;
 
-use App\Scopes\TenantScope;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Traits\BelongsToTenant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -23,24 +22,11 @@ use Illuminate\Notifications\Notifiable;
  * @property int|null $tenant_id
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
  * @property-read int|null $notifications_count
- * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|User query()
- * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereRole($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereTenantId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, BelongsToTenant;
 
     /**
      * The attributes that are mass assignable.
@@ -71,20 +57,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    /**
-     * The "booted" method of the model.
-     *
-     * @return void
-     */
-    protected static function booted()
-    {
-        static::addGlobalScope(new TenantScope);
-
-        static::creating(function ($model) {
-            if (session()->has('tenant_id')) {
-                $model->tenant_id = session()->get('tenant_id');
-            }
-        });
-    }
 }
---
+++ app/Traits/BelongsToTenant.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Tenant;
+use App\Scopes\TenantScope;
+
+trait BelongsToTenant
+{
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new TenantScope);
+
+        static::creating(function ($model) {
+            if (session()->has('tenant_id')) {
+                $model->tenant_id = session()->get('tenant_id');
+            }
+        });
+    }
+
+    public function tenant()
+    {
+        return $this->belongsTo(Tenant::class);
+    }
+}
