54c0c32e4eb7f5cf9a25adaea91acd9b1edd23d0
yxj0312 <yanxiajun@hotmail.com>
2020-11-11 20:40:06 +0100

    ajust hero layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
   Tailwind UI components require Tailwind CSS v1.8 and the @tailwindcss/ui plugin.
   Read the documentation to get started: https://tailwindui.com/documentation
 -->
-<div class="relative bg-white overflow-hidden">
+<div class="relative bg-green-50 overflow-hidden">
   <div class="max-w-screen-xl mx-auto">
-    <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-      <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none">
+    <div class="relative z-10 pb-8 bg-green-50 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+      <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="#f3faf7" viewBox="0 0 100 100" preserveAspectRatio="none">
         <polygon points="50,0 100,0 50,100 0,100" />
       </svg>
 
