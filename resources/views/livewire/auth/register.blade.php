42dbe69dd0757452e47e60fcb7d948da621473bd
yxj0312 <yanxiajun@hotmail.com>
2020-11-13 23:02:09 +0100

    use Livewire With Blade Components

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -14,15 +14,14 @@
                     <div class="mt-6">
                         {{$errors}}
                         <form wire:submit.prevent="register">
-                            <div>
-                                <label for="name" class="block text-sm font-medium leading-5 text-gray-700">
-                                    Name
-                                </label>
-                                <div class="mt-1 rounded-md shadow-sm">
-                                    <input
-                                        wire:model="name" id="name" type="text" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
-                                </div>
-                            </div>
+                            <x-text-input
+                                wire:model.debounce.100ms="name"
+                                type="text"
+                                label="Name"
+                                :required="true"
+                                placeholder="my placeholder"
+                                class="mt-4" />
+
                             <div class="mt-6">
                                 <label for="companyName" class="block text-sm font-medium leading-5 text-gray-700">
                                     Company Name
