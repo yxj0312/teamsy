ce4a4fa8e20f6db21472620aae9bc80cc6c1922d
yxj0312 <yanxiajun@hotmail.com>
2021-02-21 23:08:07 +0100

    Add alpinejs to hide modal

--- resources/views/components/confirmation-modal.blade.php
+++ resources/views/components/confirmation-modal.blade.php
@@ -1,19 +1,21 @@
-<div class="fixed inset-0 bg-gray-900 opacity-90">
-    <div class="bg-white shadow-md p-4 h-48 m-auto max-w-sm mx-auto rounded-md fixed inset-0">
-        <div class="flex flex-col h-full justify-between">
-            <header>
-                <h3 class="font-bold text-lg">
-                    {{ $title }}
-                </h3>
-            </header>
+<div x-data="{ show:false }" x-show="show">
+    <div class="fixed inset-0 bg-gray-900 opacity-90">
+        <div class="bg-white shadow-md p-4 h-48 m-auto max-w-sm mx-auto rounded-md fixed inset-0">
+            <div class="flex flex-col h-full justify-between">
+                <header>
+                    <h3 class="font-bold text-lg">
+                        {{ $title }}
+                    </h3>
+                </header>
 
-            <main class="mb-4">
-                {{ $body }}
-            </main>
+                <main class="mb-4">
+                    {{ $body }}
+                </main>
 
-            <footer>
-                {{ $footer }}
-            </footer>
+                <footer>
+                    {{ $footer }}
+                </footer>
+            </div>
         </div>
     </div>
 </div>
