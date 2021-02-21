fc247d67fe74ffa0b753b6f317ba853427d62534
yxj0312 <yanxiajun@hotmail.com>
2021-02-21 21:50:59 +0100

    Add some styles to modal and button

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -4,20 +4,22 @@
 </h1>
 
 <!-- Modal -->
-<div class="fixed inset-0 bg-gray-900 opacity-90 ">
-    <div class="bg-white shadow-md p-4 h-40 m-auto max-w-sm mx-auto rounded-md fixed inset-0">
-        <header>
-            <h3>Are You Sure?</h3>
-        </header>
+<div class="fixed inset-0 bg-gray-900 opacity-90">
+    <div class="bg-white shadow-md p-4 h-48 m-auto max-w-sm mx-auto rounded-md fixed inset-0">
+        <div class="flex flex-col h-full justify-between">
+            <header>
+                <h3 class="font-bold text-lg">Are You Sure?</h3>
+            </header>
 
-        <main>
-            <p>If you proceed, you account will be deleted entirely.</p>
-        </main>
+            <main class="mb-4">
+                <p>If you proceed, you account will be deleted entirely.</p>
+            </main>
 
-        <footer>
-            <button>Cancel</button>
-            <button>Continue</button>
-        </footer>
+            <footer>
+                <button class="bg-gray-400 text-xs uppercase py-2 px-4 rounded-md text-white hover:bg-gray-500 transition-all duration-200 mr-2">Cancel</button>
+                <butto  class="bg-blue-400 text-xs uppercase py-2 px-4 rounded-md text-white hover:bg-blue-500 transition-all duration-200">Continue</button>
+            </footer>
+        </div>
     </div>
 </div>
 </x-layout>
