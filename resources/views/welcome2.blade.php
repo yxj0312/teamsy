e466547a00cbb58ca1ae578a3ca202eb4f00e76a
yxj0312 <yanxiajun@hotmail.com>
2021-02-21 21:39:31 +0100

    More detail to modal

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -4,7 +4,20 @@
 </h1>
 
 <!-- Modal -->
-<div class="bg-white shadow-md p-4 max-w-sm mx-auto rounded-md fixed top-0">
-    <p>I am a modal.</p>
+<div class="fixed inset-0 bg-gray-900 opacity-90 ">
+    <div class="bg-white shadow-md p-4 h-40 m-auto max-w-sm mx-auto rounded-md fixed inset-0">
+        <header>
+            <h3>Are You Sure?</h3>
+        </header>
+
+        <main>
+            <p>If you proceed, you account will be deleted entirely.</p>
+        </main>
+
+        <footer>
+            <button>Cancel</button>
+            <button>Continue</button>
+        </footer>
+    </div>
 </div>
 </x-layout>
