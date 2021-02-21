a591c3f693d7add4db7c98e5cf05c8c42d7c03d2
yxj0312 <yanxiajun@hotmail.com>
2021-02-21 21:53:18 +0100

    Extra modal to component

---
+++ resources/views/components/confirmation-modal.blade.php
@@ -0,0 +1,18 @@
+<div class="fixed inset-0 bg-gray-900 opacity-90">
+    <div class="bg-white shadow-md p-4 h-48 m-auto max-w-sm mx-auto rounded-md fixed inset-0">
+        <div class="flex flex-col h-full justify-between">
+            <header>
+                <h3 class="font-bold text-lg">Are You Sure?</h3>
+            </header>
+
+            <main class="mb-4">
+                <p>If you proceed, you account will be deleted entirely.</p>
+            </main>
+
+            <footer>
+                <button class="bg-gray-400 text-xs uppercase py-2 px-4 rounded-md text-white hover:bg-gray-500 transition-all duration-200 mr-2">Cancel</button>
+                <butto  class="bg-blue-400 text-xs uppercase py-2 px-4 rounded-md text-white hover:bg-blue-500 transition-all duration-200">Continue</button>
+            </footer>
+        </div>
+    </div>
+</div>
