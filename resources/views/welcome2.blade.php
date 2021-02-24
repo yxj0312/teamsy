5f6c3492f8c3bed737715efa673ea8f8a2058d9c
yxj0312 <yanxiajun@hotmail.com>
2021-02-24 21:25:17 +0100

    Add Form layout

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -7,10 +7,16 @@
     <p class="mb-6">
         Would you like to delete your account?
     </p>
-    <p>
-        <!-- <a href="#user-delete-modal" class="underline text-blue-500">Delete</a> -->
-        <a href="#something-else-modal" class="underline text-blue-500 font-semibold">Delete</a>
-    </p>
+
+    <form action="" method="post">
+        @csrf
+
+        <p>
+            <x-button class="bg-blue-400 hover:bg-blue-500">Yes, Delete</x-button>
+            <!-- <a href="#user-delete-modal" class="underline text-blue-500">Delete</a> -->
+            <a href="#something-else-modal" class="underline text-blue-500 font-semibold">Delete</a>
+        </p>
+    </form>
 </main>
 
 <!-- Modal -->
