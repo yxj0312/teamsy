960cb29b6961ece7eb3ad08424e8c9c4bb3ae596
yxj0312 <yanxiajun@hotmail.com>
2021-02-25 21:45:37 +0100

    Move to livewire component

--- resources/views/livewire/delete-user.blade.php
+++ resources/views/livewire/delete-user.blade.php
@@ -1,3 +1,21 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day --}}
+   <form
+        id="delete-user-form"
+        action="" 
+        method="post" 
+        x-data 
+        @submit.prevent="
+            
+                location.hash = '#user-delete-modal'
+            
+        "
+    >
+        @csrf
+
+        <p>
+            <x-button class="bg-blue-400 hover:bg-blue-500">Yes, Delete</x-button>
+            <!-- <a href="#user-delete-modal" class="underline text-blue-500">Delete</a> -->
+            <a href="#something-else-modal" class="underline text-blue-500 font-semibold">Delete</a>
+        </p>
+    </form>
 </div>
--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -1,58 +1,40 @@
 <x-layout>
-<p class="mb-6">
-    Would you like to delete your account?
-</p>
-
-<form
-    id="delete-user-form"
-    action="" 
-    method="post" 
-    x-data 
-    @submit.prevent="
-        
-            location.hash = '#user-delete-modal'
-        
-    "
->
-    @csrf
-
-    <p>
-        <x-button class="bg-blue-400 hover:bg-blue-500">Yes, Delete</x-button>
-        <!-- <a href="#user-delete-modal" class="underline text-blue-500">Delete</a> -->
-        <a href="#something-else-modal" class="underline text-blue-500 font-semibold">Delete</a>
+    <p class="mb-6">
+        Would you like to delete your account?
     </p>
-</form>
 
-<!-- Modal -->
-<x-confirmation-modal name="user-delete-modal">
-    <x-slot name="title">
-        Are You Sure?
-    </x-slot>
+    @livewire('delete-user')
 
-    <x-slot name="body">
-        Continuing will delete your account permanently.
-    </x-slot>
+    <!-- Modal -->
+    <x-confirmation-modal name="user-delete-modal">
+        <x-slot name="title">
+            Are You Sure?
+        </x-slot>
 
-    <x-slot name="footer">
-        <a href="# "class="bg-gray-400 hover:bg-gray-500 text-xs uppercase py-2 px-4 rounded-md text-white transition-all duration-200">Cancel</a>
-        <!-- <x-button class="bg-gray-400 hover:bg-gray-500">Cancel</x-button> -->
-        <x-button class="bg-blue-400 hover:bg-blue-500" @click="document.querySelector('#delete-user-form').submit()">Continue</x-button>
-    </x-slot>
-</x-confirmation-modal>
+        <x-slot name="body">
+            Continuing will delete your account permanently.
+        </x-slot>
 
-<x-confirmation-modal name="something-else-modal">
-    <x-slot name="title">
-        Just Checking
-    </x-slot>
+        <x-slot name="footer">
+            <a href="# "class="bg-gray-400 hover:bg-gray-500 text-xs uppercase py-2 px-4 rounded-md text-white transition-all duration-200">Cancel</a>
+            <!-- <x-button class="bg-gray-400 hover:bg-gray-500">Cancel</x-button> -->
+            <x-button class="bg-blue-400 hover:bg-blue-500" @click="document.querySelector('#delete-user-form').submit()">Continue</x-button>
+        </x-slot>
+    </x-confirmation-modal>
 
-    <x-slot name="body">
-        I am another modal.
-    </x-slot>
+    <x-confirmation-modal name="something-else-modal">
+        <x-slot name="title">
+            Just Checking
+        </x-slot>
 
-    <x-slot name="footer">
-        <a href="#user-delete-modal "class="bg-gray-400 hover:bg-gray-500 text-xs uppercase py-2 px-4 rounded-md text-white transition-all duration-200">Retry</a>
-        <!-- <x-button class="bg-gray-400 hover:bg-gray-500">Cancel</x-button> -->
-        <x-button class="bg-blue-400 hover:bg-blue-500">Continue</x-button>
-    </x-slot>
-</x-confirmation-modal>
+        <x-slot name="body">
+            I am another modal.
+        </x-slot>
+
+        <x-slot name="footer">
+            <a href="#user-delete-modal "class="bg-gray-400 hover:bg-gray-500 text-xs uppercase py-2 px-4 rounded-md text-white transition-all duration-200">Retry</a>
+            <!-- <x-button class="bg-gray-400 hover:bg-gray-500">Cancel</x-button> -->
+            <x-button class="bg-blue-400 hover:bg-blue-500">Continue</x-button>
+        </x-slot>
+    </x-confirmation-modal>
 </x-layout>
