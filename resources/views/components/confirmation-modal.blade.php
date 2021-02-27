d0aeabe76fa094a4040d243d1887395019879498
yxj0312 <yanxiajun@hotmail.com>
2021-02-27 22:17:30 +0100

    Add some js features with alpinejs

--- resources/views/components/confirmation-modal.blade.php
+++ resources/views/components/confirmation-modal.blade.php
@@ -1,5 +1,5 @@
-<div {{ $attributes }} x-data="{ show: @entangle('showModal') }" x-show="show">
-    <div class="fixed inset-0 bg-gray-900 opacity-90">
+<div {{ $attributes }} x-data="{ show: @entangle('showModal') }" x-show="show" @keydown.escape.window="show = false">
+    <div class="fixed inset-0 bg-gray-900 opacity-90" @click="show = false">
         <div class="bg-white shadow-md p-4 h-48 m-auto max-w-sm mx-auto rounded-md fixed inset-0">
             <div class="flex flex-col h-full justify-between">
                 <header>
