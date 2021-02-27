a3f777e454546104875bb2c3bb8763ec4dba1841
yxj0312 <yanxiajun@hotmail.com>
2021-02-27 22:08:06 +0100

    share state between livewire and alpinejs

--- resources/views/livewire/delete-user.blade.php
+++ resources/views/livewire/delete-user.blade.php
@@ -7,39 +7,37 @@
         </p>
 
         <!-- Modal -->
-        @if ($showModal)
-            <x-confirmation-modal name="user-delete-modal">
-                <x-slot name="title">
-                    Are You Sure?
-                </x-slot>
+        <x-confirmation-modal name="user-delete-modal">
+            <x-slot name="title">
+                Are You Sure?
+            </x-slot>
 
-                <x-slot name="body">
-                    Continuing will delete your account permanently.
-                </x-slot>
+            <x-slot name="body">
+                Continuing will delete your account permanently.
+            </x-slot>
 
-                <x-slot name="footer">
-                    <!-- <a href="# "class="bg-gray-400 hover:bg-gray-500 text-xs uppercase py-2 px-4 rounded-md text-white transition-all duration-200">Cancel</a> -->
-                    <x-button class="bg-gray-400 hover:bg-gray-500" wire:click="$set('showModal', false)">Cancel</x-button>
-                    <x-button class="bg-blue-400 hover:bg-blue-500" wire:click="handle">Continue</x-button>
-                </x-slot>
-            </x-confirmation-modal>
+            <x-slot name="footer">
+                <!-- <a href="# "class="bg-gray-400 hover:bg-gray-500 text-xs uppercase py-2 px-4 rounded-md text-white transition-all duration-200">Cancel</a> -->
+                <x-button class="bg-gray-400 hover:bg-gray-500" wire:click="$set('showModal', false)">Cancel</x-button>
+                <x-button class="bg-blue-400 hover:bg-blue-500" wire:click="handle">Continue</x-button>
+            </x-slot>
+        </x-confirmation-modal>
 
-            <!-- <x-confirmation-modal name="something-else-modal">
-                <x-slot name="title">
-                    Just Checking
-                </x-slot>
+        <!-- <x-confirmation-modal name="something-else-modal">
+            <x-slot name="title">
+                Just Checking
+            </x-slot>
 
-                <x-slot name="body">
-                    I am another modal.
-                </x-slot>
+            <x-slot name="body">
+                I am another modal.
+            </x-slot>
 
-                <x-slot name="footer">
-                    <a href="#user-delete-modal "class="bg-gray-400 hover:bg-gray-500 text-xs uppercase py-2 px-4 rounded-md text-white transition-all duration-200">Retry</a>
-    
-                    <x-button class="bg-blue-400 hover:bg-blue-500" wire:click="handle">Continue</x-button>
-                </x-slot>
-            </x-confirmation-modal> -->
-        @endif
+            <x-slot name="footer">
+                <a href="#user-delete-modal "class="bg-gray-400 hover:bg-gray-500 text-xs uppercase py-2 px-4 rounded-md text-white transition-all duration-200">Retry</a>
+
+                <x-button class="bg-blue-400 hover:bg-blue-500" wire:click="handle">Continue</x-button>
+            </x-slot>
+        </x-confirmation-modal> -->
     </form>
 
 </div>
