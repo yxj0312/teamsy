b28b0982d58531ae5ad3797f4961742636a5c80f
yxj0312 <yanxiajun@hotmail.com>
2021-02-28 21:59:24 +0100

    transition

--- resources/views/components/confirmation-modal.blade.php
+++ resources/views/components/confirmation-modal.blade.php
@@ -5,7 +5,7 @@
     style="display:none"
 >
     <div class="fixed inset-0 bg-gray-900 opacity-90" @click="show = false">
-        <div class="bg-white shadow-md h-48 m-auto max-w-sm mx-auto rounded-md fixed inset-0">
+        <div class="bg-white shadow-md h-48 m-auto max-w-sm mx-auto rounded-md fixed inset-0" x-show.transition="show">
             <div class="flex flex-col h-full justify-between">
                 <header class="p-6">
                     <h3 class="font-bold text-lg">
