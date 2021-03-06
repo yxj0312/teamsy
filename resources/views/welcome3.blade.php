76872d601a536d42b7918f90ff499282edfe0cc8
yxj0312 <yanxiajun@hotmail.com>
2021-03-06 23:06:59 +0100

    extract modal component

--- resources/views/welcome3.blade.php
+++ resources/views/welcome3.blade.php
@@ -6,21 +6,8 @@
         </div>
     </div>
 
-    <x-modal name="join-modal">
-        <x-slot name="title">
-            <h1 class="text-lg font-bold">Join</h1>
-        </x-slot>
-
-
-        <x-slot name="body">
-            <p>Here are some details about our plans.</p>
-        </x-slot>
-
-        <x-slot name="footer">
-            <x-button class="bg-blue-500 hover:bg-blue-600" @click="show = false">Close</x-button>
-        </x-slot>
-
-    </x-modal>
+    <!-- normally put those at the bottom -->
+    <x-modals.join />
 
 
      <x-modal name="contact-modal">
